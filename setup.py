fbfdb42c31860292d28a0d7ec095da0b5d8d0ac9
xnoxs-dev <xnoxs579@gmail.com>
2024-06-20 02:38:26 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.4'
+DESCRIPTION = 'private modul'
+
+# Setting up
+setup(
+    name="xnoxs",
+    version=VERSION,
+    author="xnoxs-dev",
+    author_email="<khaerudin2119@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['xnoxs'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
